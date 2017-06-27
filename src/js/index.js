7f9456742da3e40b9539c2b64a9b8cf102050a7f
anonymous <xgqfrms@gmail.com>
2017-06-27 23:03:27 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,6 @@ import { Router, hashHistory } from 'react-router';
 import routes from './routes';
 
 ReactDOM.render(
-  <Router history={ hashHistory } routes={routes} />,
-  document.querySelector('#app')
+    <Router history={ hashHistory } routes={routes} />,
+    document.querySelector('#app')
 );
